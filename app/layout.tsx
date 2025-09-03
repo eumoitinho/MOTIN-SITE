af7cf243923b79586381a10a2ede536362c0b4c8
João Vitor <63624487+eumoitinho@users.noreply.github.com>
2025-09-03 09:28:28 -0300

    chore(layout): remove CustomWhatsAppButton for cleaner layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -126,7 +126,6 @@ export default function RootLayout({
             <Analytics />
             {children}
             <ScrollToTop />
-            <CustomWhatsAppButton />
           </ThemeProvider>
         </body>
       </html>
